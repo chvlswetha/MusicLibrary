62993acf15dcb008f1978b6a9a0ed450f2251dcd
m4rycho <103618333+m4ryc@users.noreply.github.com>
2022-06-17 14:13:55 -0700

    Added getGenres, GetAllGenres, annd GetAllMusicByGenre.

---
+++ MusicLibrary/Model/MusicManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Model
+{
+    public static class MusicManager
+    {
+        // getMusic method will give you back a List of all instances songs 
+        private static List<Music> getMusic()
+        {
+            var songs = new List<Music>();
+            songs.Add(new Music("Holizna", MusicGenre.HipHop));
+            songs.Add(new Music("Makaih", MusicGenre.HipHop));
+            songs.Add(new Music("Ice Cream", MusicGenre.Kpop));
+            songs.Add(new Music("Yet To Come", MusicGenre.Kpop));
+            songs.Add(new Music("Better", MusicGenre.RnB));
+            songs.Add(new Music("Vibin", MusicGenre.RnB));
+            songs.Add(new Music("Kinks", MusicGenre.RocknRoll));
+            songs.Add(new Music("Rolling", MusicGenre.RocknRoll));
+
+            return songs;
+        }
+
+        public static void GetAllMusic(ObservableCollection<Music> songs)
+        {
+            var allSongs = getMusic();
+            songs.Clear();
+            allSongs.ForEach(song => songs.Add(song));
+        }
+
+        private static List<Music> getGenres()
+        {
+            var genres = new List<Music>();
+            genres.Add(new Music("HipHop", MusicCategory.Genre));
+            genres.Add(new Music("Kpop", MusicCategory.Genre));
+            genres.Add(new Music("Rnb", MusicCategory.Genre));
+            genres.Add(new Music("RocknRoll", MusicCategory.Genre));
+
+            return genres;
+        }
+
+        public static void GetAllGenres(ObservableCollection<Music> genres)
+        {
+            var allGenres = getGenres();
+            genres.Clear();
+            allGenres.ForEach(genre => genres.Add(genres));
+        }
+
+
+        public static void GetAllMusicByGenre(ObservableCollection<Music> songs, MusicGenre genre)
+        {
+            var allGenres = getGenres();
+            var filteredGenres = allGenres.Where(
+                song => song.Genre == genre).ToList();
+
+            songs.Clear();
+            filteredSongs.ForEach(song => songs.Add(song));
+
+        }
+           
+
+    }
+}
